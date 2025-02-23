956a49e25b450c43ae3e42645be311ca42997b71
Devan <devandbenz@gmail.com>
2025-02-23 08:26:26 -0600

    feat: Adding tests for Page

--- src/filemanager.rs
+++ src/filemanager.rs
@@ -53,12 +53,22 @@ impl Page {
         self.byte_buffer.get(offset as usize).copied()
     }
 
-    pub fn get_bytes(&self, offset: u32) -> Option<&[u8]> {
-        self.byte_buffer.get(offset as usize..)
+    pub fn get_bytes(&self, offset: u32) -> Option<Box<[u8]>> {
+        let bytes = match self.byte_buffer.get(offset as usize..) {
+            Some(bytes) => bytes,
+            None => return None,
+        };
+
+        Some(bytes.into())
     }
 
     pub fn get_string(&self, offset: u32) -> Option<String> {
-        String::from_utf8(self.byte_buffer[offset as usize..].to_vec()).ok()
+        let bytes = match self.byte_buffer.get(offset as usize..) {
+            Some(bytes) => bytes,
+            None => return None,
+        };
+
+        Some(String::from_utf8(bytes.to_vec()).ok().unwrap().trim_end_matches('\0').to_string())
     }
 
     pub fn set_int(&mut self, offset: u32, val: Option<u8>) {
@@ -77,10 +87,6 @@ impl Page {
         if let Some(val) = val {
             self.byte_buffer[offset as usize..offset as usize + val.len()].copy_from_slice(val.as_bytes());
         }
-    }
-
-    pub(crate) fn contents(&self) -> Vec<u8> {
-        self.byte_buffer.clone()
     }
 }
 
@@ -145,7 +151,7 @@ impl FileManager {
         let mut file = self.open_file.get(block_id.file_name());
         if let Some(mut file) = file {
             file.seek(std::io::SeekFrom::Start(page.block_size as u64)).expect("seek error while reading file");
-            file.read(page.contents().as_mut_slice())?;
+            file.read(page.byte_buffer.as_mut_slice())?;
 
             Ok(())
         } else {
@@ -157,7 +163,7 @@ impl FileManager {
         let mut file = self.open_file.get(block_id.file_name());
         if let Some(mut file) = file {
             file.seek(std::io::SeekFrom::Start(page.block_size as u64)).expect("seek error while reading file");
-            file.write(page.contents().as_mut_slice())?;
+            file.write(page.byte_buffer.as_mut_slice())?;
 
             Ok(())
         } else {
@@ -200,7 +206,26 @@ mod tests {
     }
 
     #[test]
-    fn test_page() {}
+    fn test_page() {
+        let mut page = Page::builder().block_size(10).buffer(10).build();
+        assert_eq!(page.block_size(), 10);
+        assert_eq!(page.get_int(0), Some(0));
+        page.set_int(0, Some(65));
+        assert_eq!(page.get_int(0), Some(65));
+        let expected = b"A\0\0\0\0\0\0\0\0\0".to_vec().into_boxed_slice();
+        assert_eq!(page.get_bytes(0), Some(expected));
+        page.set_bytes(1, Some(b"B"));
+        let expected = b"AB\0\0\0\0\0\0\0\0".to_vec().into_boxed_slice();
+        assert_eq!(page.get_bytes(0), Some(expected));
+        page.set_bytes(0, Some(b"foobar"));
+        let expected = b"foobar\0\0\0\0".to_vec().into_boxed_slice();
+        assert_eq!(page.get_bytes(0), Some(expected));
+        assert_eq!(page.get_string(0), Some(String::from_utf8(b"foobar".to_vec()).expect("foobar is utf8")));
+
+        assert_eq!(page.get_bytes(12), None);
+        assert_eq!(page.get_int(12), None);
+        assert_eq!(page.get_string(12), None);
+    }
 
     #[test]
     fn test_file_manager() {}
