0f07962d03188406a5babac66a4a53d593c8e329
Devan <devandbenz@gmail.com>
2025-03-30 10:26:50 -0500

    feat: working on buffer struct

--- src/buffermanager.rs
+++ src/buffermanager.rs
@@ -1,19 +1,53 @@
 use crate::filemanager::{BlockId, FileManager, Page};
 use crate::logmanager::LogManager;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::atomic::{AtomicI32, Ordering};
 
 struct Buffer {
-    file_manager: FileManager,
-    log_manager: LogManager,
+    file_manager: Rc<RefCell<FileManager>>,
+    log_manager: Rc<RefCell<LogManager>>,
+    block_id: Option<BlockId>,
+    contents: Page,
+    pins: AtomicI32,
+    txn: Option<usize>,
+    lsn: Option<usize>,
 }
 
 impl Buffer {
-    pub fn new() -> Buffer {}
+    pub fn new(
+        file_manager: Rc<RefCell<FileManager>>,
+        log_manager: Rc<RefCell<LogManager>>,
+    ) -> Buffer {
+        let fm_blk_size = { file_manager.borrow_mut().block_size() };
 
-    pub fn contents() -> Page {}
+        let page = Page::builder()
+            .block_size(fm_blk_size)
+            .with_buffer()
+            .build();
+        Buffer {
+            file_manager,
+            log_manager,
+            block_id: None,
+            contents: page,
+            pins: AtomicI32::new(0),
+            txn: None,
+            lsn: None,
+        }
+    }
 
-    pub fn block_id() -> BlockId {}
+    pub fn contents(&self) -> &Page {
+        let page = &self.contents;
+        page
+    }
 
-    pub fn pinned() -> bool {}
+    pub fn block_id(&self) -> &Option<BlockId> {
+        &self.block_id
+    }
+
+    pub fn pinned(&self) -> bool {
+        self.pins.load(Ordering::Relaxed) > 0
+    }
 
     pub fn set_modified(txn: i32, lsn: i32) {}
 
